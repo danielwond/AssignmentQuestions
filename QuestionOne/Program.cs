d79e37f2d583cec09d657c7df29b1356e511b2f1
Danny <ddpro123@outlook.com>
2023-04-24 14:30:24 +0300

    Add project files.

---
+++ QuestionOne/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Write("Please Enter the hour: ");
+int hour = int.Parse(Console.ReadLine());
+Console.Write("Enter the Minutes: ");
+int minute = int.Parse(Console.ReadLine());
+
+double result = CalculateHour(hour, minute);
+
+//to find the smaller angle
+if (result > 180)
+{
+    result = 360 - result;
+}
+
+Console.WriteLine($"The Angle is {result} degrees");
+Console.ReadKey();
+
+double CalculateHour(int hour, double min)
+{
+    //the formula that we use to calculate the hour is 30 * h
+    //and the formula to calculate the minute of the hour is 0.5 * m
+
+    //and the formula to calculate the minute is 6m
+    //so we can condence it into the total.. meaning we can add the hour and the minute of the hour.. and subtract the minute from it..
+    //meaning it can be 30h + 0.5m - 6m
+
+    //hence the formula comes to 30h - 5.5m
+    var hr = (hour * 30) - 5.5 * min;
+    return hr;
+}
---
+++ QuestionTwo/Program.cs
@@ -0,0 +1,48 @@
+﻿using QuestionTwo.Models;
+
+Branch root = new();
+
+//main root
+root.branches.Add(new Branch());
+//Two on the root
+root.branches[0].branches.Add(new Branch());
+root.branches[0].branches.Add(new Branch());
+
+//one on the left of the root
+root.branches[0].branches[0].branches.Add(new Branch());
+
+//three on the second branch
+root.branches[0].branches[1].branches.Add(new Branch());
+root.branches[0].branches[1].branches.Add(new Branch());
+root.branches[0].branches[1].branches.Add(new Branch());
+
+//one on the right.. then on the left of the root
+root.branches[0].branches[1].branches[0].branches.Add(new Branch());
+
+//two on the middle.. of the right of the root..
+root.branches[0].branches[1].branches[1].branches.Add(new Branch());
+root.branches[0].branches[1].branches[1].branches.Add(new Branch());
+
+//one on the right.. of the middle.. of the right of the root..
+root.branches[0].branches[1].branches[1].branches[0].branches.Add(new Branch());
+
+int depth = CalculateDepth(root);
+Console.WriteLine("Depth of structure: " + depth);
+
+int CalculateDepth(Branch branch)
+{
+    if (branch.branches.Count == 0)
+        return 0;
+
+    int maxDepth = 0;
+
+    foreach (Branch sub in branch.branches)
+    {
+        int depth = CalculateDepth(sub);
+
+        if (depth > maxDepth)
+            maxDepth = depth;
+    }
+
+    return maxDepth + 1;
+}
